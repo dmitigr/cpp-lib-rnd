c8bedd7a0a6e562fd12e86a97c19d094f4e5adf2
Dmitry Igrishin <dmitigr@gmail.com>
2023-07-01 20:45:01 +0300

    Initial commit

---
+++ number.hpp
@@ -0,0 +1,63 @@
+// -*- C++ -*-
+//
+// Copyright 2023 Dmitry Igrishin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DMITIGR_RND_NUMBER_HPP
+#define DMITIGR_RND_NUMBER_HPP
+
+#include "exceptions.hpp"
+
+#include <chrono>
+#include <cstdlib>
+
+namespace dmitigr::rnd {
+
+// -----------------------------------------------------------------------------
+// Number generators
+// -----------------------------------------------------------------------------
+
+/// Seeds the pseudo-random number generator.
+inline void seed_by_now() noexcept
+{
+  const auto seed = std::chrono::duration_cast<std::chrono::seconds>(
+    std::chrono::system_clock::now().time_since_epoch()).count();
+  std::srand(static_cast<unsigned>(seed));
+}
+
+/**
+ * @returns The random number in range `[minimum, maximum]`.
+ *
+ * @par Requires
+ * `(minimum <= maximum)`.
+ */
+template<typename T>
+T week_integer(const T minimum, const T maximum)
+{
+  if ((minimum < 0) || !(minimum <= maximum))
+    throw Exception{"invalid interval for random number generation"};
+
+  static const auto rand_num = [](const auto up) noexcept
+  {
+    const auto num = static_cast<double>(std::rand() + 1);
+    return static_cast<T>(up * (num / RAND_MAX));
+  };
+
+  const auto range_length = maximum - minimum + 1;
+  return (rand_num(maximum + !minimum) % range_length) + minimum;
+}
+
+} // namespace dmitigr::rnd
+
+#endif  // DMITIGR_RND_NUMBER_HPP
